6ed51b6a47797df8fe8b9e25769c1c643e12a493
Yash Saraswat <yashqwe58@gmail.com>
2026-03-17 22:13:44 +0530

    feat:rate limit set to 5 request per minute

--- internal/middleware/rate_limiter.go
+++ internal/middleware/rate_limiter.go
@@ -2,11 +2,12 @@ package middleware
 
 import (
 	"net/http"
+	"time"
 
 	"golang.org/x/time/rate"
 )
 
-var limiter = rate.NewLimiter(5, 10)
+var limiter = rate.NewLimiter(rate.Every(time.Minute/5), 5)
 
 func RateLimit(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
