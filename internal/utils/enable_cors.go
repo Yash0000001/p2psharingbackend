53dc99065a7b3a46a5204dd5679a19f58fb41d31
Yash Saraswat <yashqwe58@gmail.com>
2026-04-06 19:47:21 +0530

    fix:cors fixed again for production

--- internal/utils/enable_cors.go
+++ internal/utils/enable_cors.go
@@ -5,7 +5,7 @@ import "net/http"
 func EnableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Origin", "https://p2pfrontend-tawny.vercel.app/")
+		w.Header().Set("Access-Control-Allow-Origin", "https://p2pfrontend-tawny.vercel.app")
 		// w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
