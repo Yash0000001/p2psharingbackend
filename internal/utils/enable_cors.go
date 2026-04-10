fafe598e0eb4abae9ea4709a377a9ab89086692a
Yash Saraswat <yashqwe58@gmail.com>
2026-04-10 22:01:00 +0530

    fix: cors fixed

--- internal/utils/enable_cors.go
+++ internal/utils/enable_cors.go
@@ -7,7 +7,7 @@ import (
 
 func EnableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", ""+os.Getenv("FRONTEND_PORT"))
+		w.Header().Set("Access-Control-Allow-Origin", os.Getenv("FRONTEND_PORT"))
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
